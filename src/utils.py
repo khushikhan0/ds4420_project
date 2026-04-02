b81868d8736ea12494785e2198ce0d6f7b5b20c4
Khushi Khan <khushikhan0301@gmail.com>
2026-04-02 14:39:24 -0400

    starter code for keras cnn

--- src/utils.py
+++ src/utils.py
@@ -2,6 +2,7 @@ import cv2
 from pathlib import Path
 from PIL import Image, ImageFile
 import numpy as np
+import matplotlib.pyplot as plt
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -63,3 +64,9 @@ def split_to_matrices(split_path: str, img_dim: tuple, classes: list[str]) -> np
         X.append(class_matrices)
         y.append(np.full(len(class_matrices), label))
     return np.concatenate(X), np.concatenate(y)
+
+def plot_img(x, im_shape) -> None:
+    plt.imshow(x.reshape(im_shape))
+    plt.xticks([])
+    plt.yticks([])
+    plt.gcf().set_size_inches(4, 4)
